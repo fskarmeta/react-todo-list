163cd271fd86803fa9b8a2aa1025c1d5c8b2f085
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-11-08 18:40:34 -0300

    added done or not done functionality

--- src/App.js
+++ src/App.js
@@ -8,12 +8,32 @@ import Items from "./components/items";
 
 function App() {
   const [item, setItem] = useState([
-    { text: "Buy milk" },
-    { text: "Take kids to school" },
-    { text: "Go to the post office" },
+    { text: "Buy milk", done: false },
+    { text: "Take kids to school", done: false },
+    { text: "Go to the post office", done: false },
   ]);
 
   const [current, setCurrent] = useState([{ text: "" }]);
+
+  function setDone(i) {
+    const newItems = item.map((item, idx) => {
+      if (idx === i) {
+        item.done = true;
+      }
+      return item;
+    });
+    setItem(newItems);
+  }
+
+  function setUnDone(i) {
+    const newItems = item.map((item, idx) => {
+      if (idx === i) {
+        item.done = false;
+      }
+      return item;
+    });
+    setItem(newItems);
+  }
 
   function currentText(e) {
     setCurrent({ ...current, text: e.target.value });
@@ -39,7 +59,12 @@ function App() {
         <Title />
         <ListContainer>
           <Form currentText={currentText} submitText={submitText} />
-          <Items item={item} deleteItem={deleteItem} />
+          <Items
+            item={item}
+            deleteItem={deleteItem}
+            setDone={setDone}
+            setUnDone={setUnDone}
+          />
         </ListContainer>
       </Container>
     </>
